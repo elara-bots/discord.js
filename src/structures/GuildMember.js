884511e2cfd951aae397580ffd051972b8067409
Elara Services <43923163+Elara-Discord-Bots@users.noreply.github.com>
2021-05-28 20:56:03 -0700

    Update GuildMember.js

--- src/structures/GuildMember.js
+++ src/structures/GuildMember.js
@@ -81,6 +81,7 @@ class GuildMember extends Base {
     if ('joined_at' in data) this.joinedTimestamp = new Date(data.joined_at).getTime();
     if ('premium_since' in data) this.premiumSinceTimestamp = new Date(data.premium_since).getTime();
     if ('roles' in data) this._roles = data.roles;
+    this.pending = data.pending ?? false;
   }
 
   _clone() {
