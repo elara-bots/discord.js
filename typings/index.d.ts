f9dfa4592b881ab3f59ec4daa0799ab2f8c2329f
Elara Services <43923163+Elara-Discord-Bots@users.noreply.github.com>
2021-07-04 09:43:28 -0700

    Add support for `User.banner`

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -627,13 +627,20 @@ declare module 'discord.js' {
           hash: string,
           format: 'default' | AllowedImageFormat,
           size: number,
+          dynamic: boolean,
         ) => string;
-        Banner: (guildID: Snowflake | number, hash: string, format: AllowedImageFormat, size: number) => string;
+        Banner: (
+          id: Snowflake | number,
+          hash: string,
+          format: AllowedImageFormat,
+          size: number,
+          dynamic: boolean) => string;
         Icon: (
           userID: Snowflake | number,
           hash: string,
           format: 'default' | AllowedImageFormat,
           size: number,
+          dynamic: boolean,
         ) => string;
         AppIcon: (userID: Snowflake | number, hash: string, format: AllowedImageFormat, size: number) => string;
         AppAsset: (userID: Snowflake | number, hash: string, format: AllowedImageFormat, size: number) => string;
@@ -2016,6 +2023,7 @@ declare module 'discord.js' {
   export class User extends PartialTextBasedChannel(Base) {
     constructor(client: Client, data: unknown);
     public avatar: string | null;
+    public banner: string | null;
     public bot: boolean;
     public readonly createdAt: Date;
     public readonly createdTimestamp: number;
@@ -2031,6 +2039,7 @@ declare module 'discord.js' {
     public readonly tag: string;
     public username: string;
     public avatarURL(options?: ImageURLOptions): string | null;
+    public bannerURL(options?: ImageURLOptions): string | null;
     public createDM(): Promise<DMChannel>;
     public deleteDM(): Promise<DMChannel>;
     public displayAvatarURL(options?: ImageURLOptions): string;
